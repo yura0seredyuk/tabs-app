f0b18a8bb0de0126097e5e2b4eecae769ed7063f
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-04-20 23:29:32 +0300

    add tabs and some styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import { tabsList } from './tabsList';
 
 function App() {
+  const [tabs, setTabs] = useState([]);
+  const [currentTabId, setCurrentTabId] = useState(2);
+  const [currentTabTitle, setCurrentTabTitle] = useState('');
+
+  useEffect(() => {
+    setTabs(tabsList);
+  }, []);
+
+  const choseTabId = (id) => {
+    setCurrentTabId(tabs.find(tab => tab.id === id).id);
+    setCurrentTabTitle(tabs.find(tab => tab.id === id).title);
+  };
+
   return (
-    <div>
-      123
+    <div className="container">
+      <div className='tab'>
+        <nav className='tab__container'>
+          <ul className="tab__list">
+            {tabs.map(tab => (
+              <li
+                type='button'
+                className={
+                  currentTabId === tab.id
+                    ? "tab__item__active tab__item"
+                    : "tab__item"
+                }
+                key={tab.id}
+                onClick={() => choseTabId(tab.id)}
+              >
+                <a className='tab__link'>{tab.title}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <div className='tab__text'>
+          {currentTabTitle}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/tabsList.js
@@ -0,0 +1,13 @@
+export const tabsList = [
+  { id: 1, title: 'Job Focus', content: 'Some text 1' },
+  { id: 2, title: 'Soft Skills', content: 'Some text 2' },
+  { id: 3, title: 'Technical Skills', content: 'Some text 3' },
+  { id: 4, title: 'Functional Expertise', content: 'Some text 4' },
+  { id: 5, title: 'Domain Expertise', content: 'Some text 5' },
+  { id: 6, title: 'Patent Expertise', content: 'Some text 6' },
+  { id: 7, title: 'Personal Expertise', content: 'Some text 7' },
+  { id: 8, title: 'Hard Expertise', content: 'Some text 8' },
+  { id: 9, title: 'Domain Expertise', content: 'Some text 9' },
+  { id: 10, title: 'Domain Expertise', content: 'Some text 10' },
+  { id: 11, title: 'Domain Expertise', content: 'Some text 11' },
+];
