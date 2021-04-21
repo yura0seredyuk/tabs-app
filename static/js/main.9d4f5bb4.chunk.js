b5d45ec9c4ca5fce95c38223d043f790c919e501
yura0seredyuk <yura.seredyuk0@gmail.com>
2021-04-21 15:21:43 +0300

    Updates

--- static/js/main.9d4f5bb4.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonptabs-app"]=this["webpackJsonptabs-app"]||[]).push([[0],{11:function(t,n,e){"use strict";e.r(n);var c=e(1),r=e.n(c),s=e(3),i=e.n(s),a=(e(8),e(9),e(0));var o=function(){return Object(a.jsx)("div",{children:"123"})};i.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(o,{})}),document.getElementById("root"))},8:function(t,n,e){},9:function(t,n,e){}},[[11,1,2]]]);
-//# sourceMappingURL=main.9d4f5bb4.chunk.js.map
